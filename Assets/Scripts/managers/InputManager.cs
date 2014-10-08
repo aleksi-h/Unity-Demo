c3530e7114a1015f5843151cd6a43cf516b97c1c
aleksi-h <aleksi-h@users.noreply.github.com>
2014-10-08 19:16:20 +0300

    changed camera

--- Assets/Scripts/managers/InputManager.cs
+++ Assets/Scripts/managers/InputManager.cs
@@ -14,8 +14,20 @@ public class InputManager : Singleton<InputManager> {
         //**********************rotating**********************
         if (Input.touchCount == 2)
         {
-            Touch finger1 = Input.GetTouch(0);
-            Touch finger2 = Input.GetTouch(1);
+            Touch finger1;
+            Touch finger2;
+
+            //finger1 is always on the left, to keep turn direction constant, regardless of which finger touched the screen first
+            if (Input.GetTouch(0).position.x < Input.GetTouch(1).position.x)
+            {
+                finger1 = Input.GetTouch(0);
+                finger2 = Input.GetTouch(1);
+            }
+            else
+            {
+                finger1 = Input.GetTouch(1);
+                finger2 = Input.GetTouch(0);
+            }
 
             if (finger1.phase == TouchPhase.Moved || finger2.phase == TouchPhase.Moved)
             {
@@ -34,7 +46,14 @@ public class InputManager : Singleton<InputManager> {
         else if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-            mainCamera.transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+            //mainCamera.transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+
+            float originalX = mainCamera.transform.eulerAngles.x;
+            float originalZ = mainCamera.transform.eulerAngles.z;
+            mainCamera.transform.Rotate(new Vector3(-originalX, 0, -originalZ)); //remove X and Z rotation from the camera to retain it's height when moved
+            mainCamera.transform.Translate(-touchDeltaPosition.x * speed, 0, -touchDeltaPosition.y * speed); //move the camera
+            mainCamera.transform.Rotate(new Vector3(originalX, 0, originalZ)); //reapply X and Z rotations
+            
         }
         //**********************tapping**********************
         else if (Input.touchCount == 1)
