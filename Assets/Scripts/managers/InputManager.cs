a5ad457ccb6600c07e48ba492085d825c13eb97a
aleksi-h <aleksi-h@users.noreply.github.com>
2014-10-13 18:46:31 +0300

    added zoom

--- Assets/Scripts/managers/InputManager.cs
+++ Assets/Scripts/managers/InputManager.cs
@@ -1,65 +1,113 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InputManager : Singleton<InputManager>
-{
+public class InputManager : Singleton<InputManager> {
     public delegate void TapEvent(Vector3 tapPos);
     public static event TapEvent OnTap;
 
-    public delegate void DragEvent(Touch t);
+    public delegate void DragEvent(Vector2 deltaPos);
     public static event DragEvent OnDrag;
 
-    public delegate void RotateEvent(Touch t1, Touch t2);
+    public delegate void RotateEvent(float amount);
     public static event RotateEvent OnRotate;
 
-    private bool idle;
+    public delegate void PinchEvent(float amount);
+    public static event PinchEvent OnPinch;
 
-    void Update()
-    {
-        //rotate
-        if (Input.touchCount == 2)
-        {
-            idle = false;
-            if (OnRotate != null)
-            {
-                OnRotate(Input.GetTouch(0), Input.GetTouch(1));
-            }
-        }
-        //drag
-        else if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            idle = false;
-            if (OnDrag != null)
-            {
-                OnDrag(Input.GetTouch(0));
-            }
-        }
-        //tap
-        else if (Input.touchCount == 1 && idle)
-        {
-            if (Input.GetTouch(0).phase == TouchPhase.Ended && Input.GetTouch(0).tapCount == 1)
-            {
-                if (OnTap != null)
-                {
+    private State state;
+    private enum State {
+        IDLE, PINCHING, ROTATING, DRAGGING, TAPPING
+    }
+
+    void Update() {
+        if (state == State.IDLE) { UpdateState(); }
+
+        switch (state) {
+            case State.PINCHING:
+                if (Input.touchCount < 2) { state = State.IDLE; }
+                else if (OnPinch != null) {
+                    Touch t0 = Input.GetTouch(0);
+                    Touch t1 = Input.GetTouch(1);
+                    Vector2 t0PrevPos = t0.position - t0.deltaPosition;
+                    Vector2 t1PrevPos = t1.position - t1.deltaPosition;
+                    float prevFrameDeltaMag = (t0PrevPos - t1PrevPos).magnitude;
+                    float deltaMag = (t0.position - t1.position).magnitude;
+                    float deltaMagnitudeDiff = prevFrameDeltaMag - deltaMag;
+                    OnPinch(deltaMagnitudeDiff);
+                }
+                break;
+            case State.ROTATING:
+                if (Input.touchCount < 2) { state = State.IDLE; }
+                else if (OnRotate != null) {
+                    Touch t0 = Input.GetTouch(0);
+                    Touch t1 = Input.GetTouch(1);
+
+                    //make sure that leftmost touch is t0
+                    if (t0.position.x > t1.position.x) {
+                        Touch aux = t0;
+                        t0 = t1;
+                        t1 = aux;
+                    }
+                    float t0DistanceMoved = t0.deltaPosition.y;
+                    float t1DistanceMoved = -t1.deltaPosition.y;
+                    float combinedDistanceMoved = t0DistanceMoved + t1DistanceMoved;
+                    OnRotate(combinedDistanceMoved);
+                }
+                break;
+            case State.DRAGGING:
+                if (Input.touchCount != 1) { state = State.IDLE; }
+                else if (OnDrag != null) { OnDrag(Input.GetTouch(0).deltaPosition); }
+                break;
+            case State.TAPPING:
+                if (OnTap != null) {
                     Touch t = Input.GetTouch(0);
                     OnTap(new Vector3(t.position.x, t.position.y, 0));
                 }
+                state = State.IDLE;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void UpdateState() {
+        if (Input.touchCount == 2) {
+            Touch t0 = Input.GetTouch(0);
+            Touch t1 = Input.GetTouch(1);
+            if (t0.phase == TouchPhase.Moved && t1.phase == TouchPhase.Moved) {
+                Vector2 t0PrevPos = t0.position - t0.deltaPosition;
+                Vector2 t1PrevPos = t1.position - t1.deltaPosition;
+                float prevDistance = Vector2.Distance(t0PrevPos, t1PrevPos);
+                float distance = Vector2.Distance(t0.position, t1.position);
+                float deltaDistance = prevDistance - distance;
+
+                float prevAngle = Vector2.Angle(t0PrevPos - t1PrevPos, Vector2.up);
+                float angle = Vector2.Angle(t0.position - t1.position, Vector2.up);
+                float deltaAngle = Mathf.DeltaAngle(prevAngle, angle);
+                Debug.Log("a1 " + prevAngle + "  |  a2 " + angle);
+
+                float ratio = Mathf.Abs(deltaAngle) / Mathf.Abs(deltaDistance);
+                Debug.Log("ratio " + ratio + "  |  " + deltaAngle + " / " + deltaDistance);
+
+                if (ratio < 0.17f) { state = State.PINCHING; }
+                else { state = State.ROTATING; }
+            }
+        }
+        else if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            state = State.DRAGGING;
+        }
+        else if (Input.touchCount == 1) {
+            if (Input.GetTouch(0).phase == TouchPhase.Ended && Input.GetTouch(0).tapCount == 1) {
+                state = State.TAPPING;
             }
         }
 #if UNITY_EDITOR
-        else if (Input.GetMouseButtonDown(0))
-        {
-            if (OnTap != null)
-            {
+        else if (Input.GetMouseButtonDown(0)) {
+            if (OnTap != null) {
                 //OnTap(Input.mousePosition);
             }
         }
 #endif
-
-        if (Input.touchCount == 0) { idle = true; }
     }
-
-#if UNITY_EDITOR
-#endif
 }
 
